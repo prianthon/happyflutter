4ba07214d41cf8967389c48dd1a35e977c9810a6
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-04-03 15:45:48 +0700

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,16 @@ class Application extends StatefulWidget {
 }
 
 class _ApplicationState extends State<Application> {
-
-  List<int> itemgrid = new List();
-
-  @override
-  void initState() {
-    for (int i = 0; i < 30; i++)
-      itemgrid.add(i);
-  }
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new GridView.builder(
-        itemCount: itemgrid.length,
-        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4),
-        itemBuilder: (BuildContext context ,int
-            index) {
-          return new Card(
-          color: Colors.blue,
-          child: new Padding(padding: const EdgeInsets.all(25.0)),
-          );
-          })
+      appBar: new AppBar(
+        backgroundColor: Colors.orange,
+        title: new Text('AppBar Widget'),
+        titleSpacing: 80.0,
+        elevation: 35.0,
+        toolbarOpacity: 0.5,
+      ),
     );
   }
 }
