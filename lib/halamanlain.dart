da217f8f1141e8324db765e5489d634c01550ecb
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-04-04 13:47:49 +0700

    widget route

---
+++ lib/halamanlain.dart
@@ -0,0 +1,18 @@
+// route widget
+import 'package:flutter/material.dart';
+
+class pagebaru extends StatelessWidget {
+
+  final String menu;
+  pagebaru(this.menu);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(title: new Text('$menu'),),
+      body: new Center(
+        child: new Text('$menu'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-// drawer widget route
+// Routes Widget
 import 'package:flutter/material.dart';
+import 'halamanlain.dart';
 
 void main() {
   runApp(new MaterialApp(home: application()));
@@ -12,7 +13,7 @@ class application extends StatefulWidget {
 
 class _applicationState extends State<application> {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return new Scaffold(
       appBar: AppBar(
         title: new Text('Navigasi Drawer'),
@@ -22,16 +23,28 @@ class _applicationState extends State<application> {
         child: new ListView(
           children: <Widget>[
             new UserAccountsDrawerHeader(
-              accountName: Text('Pri Anton Subardio'),
-              accountEmail: Text('ceo@mutiarasoka.id'),
-            currentAccountPicture: new CircleAvatar(backgroundColor: Colors.black26, child: new Text('P'),),
-            decoration: new BoxDecoration(color: Colors.lightBlue), otherAccountsPictures: <Widget>[
-              new CircleAvatar(backgroundColor: Colors.black26, child: new Text('Y'),),
-              new CircleAvatar(backgroundColor: Colors.black26, child: new Text('W'),),
-            ],),
-            new ListTile(title: new Text('Home Page'), trailing: new Icon(Icons.home),),
-            new ListTile(title: new Text('List Laptop'), trailing: new Icon(Icons.laptop),),
-            new ListTile(title: new Text('Close'), trailing: new Icon(Icons.close),onTap: (){Navigator.pop(context);},),
+                accountName: Text('Pri Anton Subardio'),
+                accountEmail: Text('ceo@mutiarasoka.id'),
+                currentAccountPicture: new CircleAvatar(backgroundColor: Colors.black26, child: new Text('P'),),
+                decoration: new BoxDecoration(color: Colors.lightBlue),
+                otherAccountsPictures: <Widget>[
+                new CircleAvatar(backgroundColor: Colors.black26, child: new Text('Y'),),
+                new CircleAvatar(backgroundColor: Colors.black26, child: new Text('W'),),
+                ],
+      ),
+                new ListTile(title: new Text('Home Page'),
+                  trailing: new Icon(Icons.home),
+                    onTap: () => Navigator.of(context).push(new
+                    MaterialPageRoute(builder: (BuildContext context) => new pagebaru('Welcome to Home Page'))),
+                ),
+                new ListTile(title: new Text('List Laptop'),
+                  trailing: new Icon(Icons.laptop),
+                  onTap: () => Navigator.of(context).push(new
+                  MaterialPageRoute(builder: (BuildContext context) => new pagebaru('Welcome to My List Product'))),
+                ),
+                new ListTile(title: new Text('Close'),
+                  trailing: new Icon(Icons.close),
+                  onTap: (){Navigator.pop(context);},),
           ],
         ),
       ),
