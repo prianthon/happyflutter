78fc1f4c17b8857d3434c9b85a9cf3a6c17a0eb3
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2020-04-07 09:46:32 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,127 @@
 // snackbar
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-void main() {
-  runApp(new MaterialApp(home: MyApp()));
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  @override
+  initState() {
+    super.initState();
+  }
 
-  final GlobalKey<ScaffoldState> skey = new
-  GlobalKey<ScaffoldState>();
+  void showLongToast() {
+    Fluttertoast.showToast(
+      msg: "This is Long Toast",
+      toastLength: Toast.LENGTH_LONG,
+    );
+  }
 
-  void method1() {
-    skey.currentState.showSnackBar(
-        new SnackBar(content: new Text('this is Snackbar')));
+  void showWebColoredToast() {
+    Fluttertoast.showToast(
+      msg: "This is Colored Toast with android duration of 5 Sec",
+      toastLength: Toast.LENGTH_SHORT,
+      webBgColor: "#e74c3c",
+      timeInSecForIosWeb: 5,
+    );
+  }
+
+  void showColoredToast() {
+    Fluttertoast.showToast(
+      msg: "This is Colored Toast with android duration of 5 Sec",
+      toastLength: Toast.LENGTH_SHORT,
+      backgroundColor: Colors.red,
+      textColor: Colors.white);
+  }
+
+  void showShortToast() {
+    Fluttertoast.showToast(
+      msg: "This is Short Toast",
+      toastLength: Toast.LENGTH_SHORT,
+      timeInSecForIosWeb: 1);
+  }
+
+  void showTopShortToast() {
+    Fluttertoast.showToast(
+      msg: "This is Top Short Toast",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.TOP,
+      timeInSecForIosWeb: 1);
+  }
+
+  void showCenterShortToast() {
+    Fluttertoast.showToast(
+      msg: "This is Center Short Toast",
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1);
+  }
+
+  void cancelToast() {
+    Fluttertoast.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Snackbar'),
-      ),
-      key: skey,
-      body: new Center(
-        child: new RaisedButton(onPressed: () {
-          method1();
-        },
-          child: new Text('Snackbar'),),
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Flutter Toast'),
+        ),
+        body: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new RaisedButton(
+                  child: new Text('Show Long Toast'),
+                  onPressed: showLongToast),
+                ),
+                new Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: new RaisedButton(
+                    child: new Text('Show Short Toast'),
+                    onPressed: showShortToast),
+                  ),
+                new Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: new RaisedButton(
+                    child: new Text('Show Center Short Toast'),
+                    onPressed: showCenterShortToast),
+                  ),
+                new Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: new RaisedButton(
+                    child: new Text('Show Top Short Toast'),
+                    onPressed: showTopShortToast),
+                  ),
+                new Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: new RaisedButton(
+                    child: new Text('Show Colored Toast'),
+                    onPressed: showColoredToast),
+                  ),
+                new Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: new RaisedButton(
+                    child: new Text('Show Web Colored Toast'),
+                    onPressed: showWebColoredToast),
+                  ),
+                new Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: new RaisedButton(
+                    child: new Text('Cancel Toasts'),
+                    onPressed: cancelToast,
+                  ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
